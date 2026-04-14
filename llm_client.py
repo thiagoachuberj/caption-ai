f29d043cddfa453877b8812a99125e3308e868e0
Thiago Silva <thiago.achuberj@gmail.com>
2026-04-14 19:50:38 -0300

    getting open-api-key in secrets

--- llm_client.py
+++ llm_client.py
@@ -2,10 +2,11 @@ import json
 from openai import OpenAI
 import os
 from dotenv import load_dotenv
+import streamlit as st
 
 load_dotenv()
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY") or st.secrets["OPENAI_API_KEY"]
 
 if not OPENAI_API_KEY:
   raise RuntimeError("OPEN_API_KEY não encontrada no ambiente.")
